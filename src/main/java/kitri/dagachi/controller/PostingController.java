74c099e56f5511abfc33b1367071ebce1df0358f
MIMIMIMIRARA <cyzhcilk@gmail.com>
2022-11-04 14:28:49 +0900

    미라 컨트롤러

--- src/main/java/kitri/dagachi/controller/PostingController.java
+++ src/main/java/kitri/dagachi/controller/PostingController.java
@@ -1,4 +1,17 @@
 package kitri.dagachi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
 public class PostingController {
+    @GetMapping("/dagachi/daca")
+    public String dagachi(Model model)
+    {
+
+        return "enterPosting";
+
 }
