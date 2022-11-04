2c7590eed672a2d85bf1b042e1fa46725ca25c4b
leedahye426 <9535957@naver.com>
2022-11-04 14:14:00 +0900

    다혜

--- src/main/java/kitri/dagachi/controller/PostingController.java
+++ src/main/java/kitri/dagachi/controller/PostingController.java
@@ -1,4 +1,13 @@
 package kitri.dagachi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostingController {
+
+    @GetMapping("/enter/enter_post")
+    public String project() {
+        return "enterPosting";
+    }
 }
