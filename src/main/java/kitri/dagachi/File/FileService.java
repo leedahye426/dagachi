a141b50c8b43057143846c4e3e8a04cd8246db6f
MIMIMIMIRARA <cyzhcilk@gmail.com>
2022-11-08 17:59:11 +0900

    '11/8'

---
+++ src/main/java/kitri/dagachi/File/FileService.java
@@ -0,0 +1,63 @@
+package kitri.dagachi.File;
+
+import lombok.RequiredArgsConstructor;
+//import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@RequiredArgsConstructor
+@Service
+public class FileService{
+                private final FileRepository fileRepository;
+        @Value("${file.dir}")
+
+                private String fileDir;
+
+
+
+                public Long saveFile(MultipartFile files) throws IOException
+        {
+                        if(files.isEmpty()){
+                                return null;
+                        }
+
+                // 원래 파일 이름 추출
+                String origName = files.getOriginalFilename();
+
+                // 파일 이름으로 쓸 uuid 생성
+                String uuid = UUID.randomUUID().toString();
+
+                // 확장자 추출(ex : .png)
+                String extension = origName.substring(origName.lastIndexOf("."));
+
+                // uuid와 확장자 결합
+                String savedName = uuid + extension;
+
+                // 파일을 불러올 때 사용할 파일 경로
+                String savedPath = fileDir + savedName;
+
+                // 파일 엔티티 생성
+                FileForm file = FileForm.builder()
+                        .origName(origName)
+                        .savedNm(savedName)
+                        .savedPath(savedPath)
+                        .build();
+
+                // 실제로 로컬에 uuid를 파일명으로 저장
+                files.transferTo(new File(savedPath));
+
+                // 데이터베이스에 파일 정보 저장
+                FileForm savedFile = fileRepository.save(file);
+
+                return savedFile.getId();
+        }
+        }
+
--- src/main/java/kitri/dagachi/controller/PostingController.java
+++ src/main/java/kitri/dagachi/controller/PostingController.java
@@ -1,33 +1,223 @@
 package kitri.dagachi.controller;
 
+import kitri.dagachi.model.Post;
+//import kitri.dagachi.service.FileService;
+import kitri.dagachi.repository.PostRepository;
+import kitri.dagachi.service.postService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 @Controller
-//@RequestMapping
+@RequiredArgsConstructor
+@RequestMapping
 public class PostingController {
+
+    private final postService postservice;
+    private final PostRepository postRepository;
+
+
+//    public String posting(Model model) {
+//        model.addAttribute("form", new PostForm());
+
     @GetMapping("/enter/enter_post")
-    public String posting(Model model) {
-        model.addAttribute("form", new PostForm());
-
-        return "enterPosting";
-
+    public String posting(){
+        return "/post/enterPosting";
     }
-}
 
 //    @PostMapping("/enter/enter_post")
-//            public String addImage(@RequestParam("photo")MultipartFile uploadFile, HttpServletRequest request)
-//    {
+//    public String posting(Model model){
+//        model.addAttribute("form", new PostForm());
+//        return "/post/enterPosting";
+//    }
+
+    @GetMapping("/enter/register")
+    public String enter_register() {
+
+        return "/post/enter_register";
+
+    }
+
+    @PostMapping("/enter/enter_post")
+    public String enter_resister(PostForm postForm) {
+
+//        post.setPosting_Content(post.getPosting_Content());
+//        post.setPosting_Title(post.getPosting_Title());
+//      post.setUpload_Date(post.getUpload_Date());
+//        post.setCompany_Name(post.getCompany_Name());
+//        post.setMember_Id(post.getMember_Id());
+//     System.out.println(postForm.getCompany_Name());
+
+
+//        postRepository.findAll();
+        postservice.savePost(postForm);
+        postservice.findPostings();
+        return "/post/enterPosting";
+    }
+
+
+//        @PostMapping("enter/register")
+//    public String updateItem(@ModelAttribute("form") PostForm form) {
 //
-//        return "업로드 완료"
+//        postservice.upload();
+//
+//        return "/post/enterPosting";
+//    }
+
+
+    @PostMapping("enter/register")
+    public String updateItem(Model model) {
+        model.addAttribute("form", new PostForm());
+
+
+        postservice.upload();
+
+        return "/post/enterPosting";
+    }
+
+
+//    @PostMapping("enter/register")
+//    public String updateItem(@PathVariable Long Posting_ID, @ModelAttribute("form") PostForm form) {
+//
+//        postservice.findPostings(Posting_ID, form.getCompany_Name(), form.Posting_Content(), form.getPosting_Title(),form.getUpload_Date(),form.getMember_ID());
+//
+//        return "/post/enterPosting";
+//    }
+
+//
+//        postForm.setPosting_ID(postForm.getPosting_ID());
+//        postForm.setPosting_Content(postForm.getPosting_Content());
+//        postForm.setPosting_Title(postForm.getPosting_Title());
+//        postForm.setUpload_Date(postForm.getUpload_Date());
+//        postForm.setCompany_Name(postForm.getCompany_Name());
+//        postForm.setMember_ID(postForm.getMember_ID());
+
+//        postservice.savePost(postForm);
+
+}
+
+
+
+ /*   @GetMapping("/enter/post")
+    public String createForm(Model model) {
+        model.addAttribute("PostForm", new PostForm());
+        return "/enter/";
+    }*/
+
+
+
+//    @GetMapping("/enter/enter_post")
+//    public String list(Model model) {
+//        List<PostForm> postForms = postservice.findPostings();
+//        model.addAttribute("postForm", postForms);
+//        return "post/enterPosting";
+//    }
+//}
+
+//    @PostMapping("/enter/")
+//            public String create(PostForm postForm)
+//    {
+//        postForm.setPosting_ID(postForm.getPosting_ID());
+//        postForm.setPosting_Content(postForm.getPosting_Content());
+//        postForm.setPosting_Title(postForm.getPosting_Title());
+//        postForm.setUpload_Date(postForm.getUpload_Date());
+//        postForm.setCompany_Name(postForm.getCompany_Name());
+//        postForm.setMember_ID(postForm.getMember_ID());
+//     System.out.println(postForm.getCompany_Name());
+//} postForm.setPosting_ID(postForm.getPosting_ID());
+
+
+
+
+
+
+
+//    @RequestMapping("/enter/register")
+//    @PostMapping("/enter/enter_post")
+
+//            public String submit(HttpServletRequest httpServletRequest, Model model)
+//    {
+//        String Company_Name = httpServletRequest.getParameter("Company_Name");
+//        String Upload_Date = httpServletRequest.getParameter("Upload_Date");
+//        String Posting_Content = httpServletRequest.getParameter("Posting_Content");
+//        String Posting_Title = httpServletRequest.getParameter("Posting_Title");
+//        String Member_Id =httpServletRequest.getParameter("Member_Id");
+//
+//        model.addAttribute("Company_Name",Company_Name);
+//        model.addAttribute("Upload_Date",Upload_Date);
+//        model.addAttribute("Posting_Content",Posting_Content);
+//        model.addAttribute("Posting_Title",Posting_Title);
+//        model.addAttribute("Member_Id",Member_Id);
+//
+//        //insert
+//
+//
+//
+//        return "/post/enterPosting";
+//    }
+//}
+
+
+//        Post post = new Post();
+//        post.setCompany_Name(form.getCompany_Name());
+//        post.setPosting_Content(form.getPosting_Content());
+////        post.setPosting_Title(form.getPosting_Title());
+////        post.setMember_Id(form.getMember_ID());
+//          post.setUpload_Date(form.getUpload_Date());
+//
+//
+//
+//        postForm.setPosting_ID(postForm.getPosting_ID());
+//        postForm.setPosting_Content(postForm.getPosting_Content());
+//        postForm.setPosting_Title(postForm.getPosting_Title());
+//        postForm.setUpload_Date(postForm.getUpload_Date());
+//        postForm.setCompany_Name(postForm.getCompany_Name());
+//        postForm.setMember_ID(postForm.getMember_ID());
+//        System.out.println(postForm.getCompany_Name());
+//
+
+
+
+//    @GetMapping("/enter/post")
+//    public String list(Model model) {
+//        List<Post> post = postService.findPost();
+//        model.addAttribute("post",post);
+//    return "post/enterPosting";
+//    }
+//}
+
+
+//    @GetMapping("/enter/enter_post")
+//    public String register() {
+//
+//
+//        return "/post/enter_register";
+//
+//    }
+
+
+//    @PostMapping("/enter/enter_post")
+//            public String addImage(@RequestParam PostForm form, HttpServletRequest request)
+//    {
+//        Post post = new Post();
+//        post.setCompany_Name(form.getCompany_Name());
+//        post.setUpload_Date(form.getUpload_Date());
+//        post.setPosting_Content(form.getPosting_Content());
+//        post.setPosting_Title(form.getPosting_Title());
+//        post.setMember_Id(form.getMember_ID());
+//
+//        return "업로드 완료";
 //    }
 //}
 
