c2ecbc13e6bf19d8d391a574125b2e7e97eea1bd
leedahye426 <9535957@naver.com>
2022-12-01 17:55:49 +0900

    12/1

--- src/main/java/kitri/dagachi/WebConfig.java
+++ src/main/java/kitri/dagachi/WebConfig.java
@@ -10,12 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     private String uploadPath = "/upload/**";
     private String portfolioResourcePath = "file:///D:/test/portfolio/";
     private String posterResourcePath = "file:///D:/test/poster/";
+    private String bannerResourcePath = "file:///D:/test/banner/";
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //요청 url이 /upload/로 시작될 경우 D:/test/poster로 요청을 전달
         registry.addResourceHandler(uploadPath)
                 .addResourceLocations(portfolioResourcePath)
+                .addResourceLocations(bannerResourcePath)
                 .addResourceLocations(posterResourcePath);
     }
 }
